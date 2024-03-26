5f5b6e61e591102d1dd4ef20edc1e08b957d2357
Owen Lewis <owenalewis@gmail.com>
2024-03-26 11:49:50 +0100

    [feat] Add Personal.jsx for the personal section - Add a file for creating the personal section of the CV application. - Add pseudocode to file to outline initial requirements.

---
+++ src/Personal.jsx
@@ -0,0 +1,26 @@
+/*
+PSEUDOCODE
+
+Name:
+- Placeholder for name with edit button.
+- Make mandatory field when editing.
+- Label for name (first and last or both in one).
+- Input for name (first and last or both in one).
+- State used to tell when editing.
+- When editing the input should appear under the name or the name
+should become editable.
+- When not editing the inputs should disappear.
+- State used to update name on form in real time.
+
+Email:
+- Placeholder for email with edit button.
+- Make mandatory field when editing.
+- Label email.
+- Input for email.
+- State used to show edit button when hovering over email.
+- State used to tell when editing.
+  - When editing the input should appear under the email or the field
+    should become editable.
+  - When not editing the inputs should disappear.
+- State used to update email on form in real time.
+*/
