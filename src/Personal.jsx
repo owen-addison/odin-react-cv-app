93d04bc04c4fbacd4e4a1d87aa0b87e67be59f79
Owen Lewis <owenalewis@gmail.com>
2024-03-27 12:05:28 +0100

    [feat] Create and export Personal component - Create a personal component. - Populate component with sections and placeholders for each item in   each container section. - Export the component.

--- src/Personal.jsx
+++ src/Personal.jsx
@@ -1,3 +1,29 @@
+function Personal() {
+  return (
+    <>
+      <div className="personal-section">
+        <div className="container">
+          <h1 id="name">John Daniel Smith</h1>
+        </div>
+        <div className="container">
+          <ul className="contact-list">
+            <li id="email">john-daniel-smith@me.com</li>
+            <li id="phone-number">+44 (0) 7897897897</li>
+            <li id="website">www.john-d-smith.me</li>
+          </ul>
+        </div>
+        <div className="container">
+          <p id="about">
+            Brief introduction of yourself and your motivations...
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Personal;
+
 /*
 PSEUDOCODE
 
