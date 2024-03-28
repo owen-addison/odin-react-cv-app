dbd9c1054d452932564acedaf8bdd71b05c39a67
Owen Lewis <owenalewis@gmail.com>
2024-03-28 14:43:39 +0100

    [tooling] Add custom tailwind width and height

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        a4: '210mm',
+      },
+      height: {
+        a4: '297mm',
+      },
+    },
   },
   plugins: [],
 };
