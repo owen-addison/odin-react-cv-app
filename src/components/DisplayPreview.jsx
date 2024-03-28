1bd09a8582e4fd55e18b676fbdb78a725258606d
Owen Lewis <owenalewis@gmail.com>
2024-03-28 15:21:07 +0100

    [style] Add div for CV preview and add tw classes

--- src/components/DisplayPreview.jsx
+++ src/components/DisplayPreview.jsx
@@ -1,21 +1,23 @@
 function DisplayPreview() {
   return (
     <>
-      <div className="personal-section">
-        <div className="container">
-          <h1 id="name">John Daniel Smith</h1>
-        </div>
-        <div className="container">
-          <ul className="contact-list">
-            <li id="email">john-daniel-smith@me.com</li>
-            <li id="phone-number">+44 (0) 7897897897</li>
-            <li id="website">www.john-d-smith.me</li>
-          </ul>
-        </div>
-        <div className="container">
-          <p id="about">
-            Brief introduction of yourself and your motivations...
-          </p>
+      <div id="cv-container" className="w-a4 h-a4 bg-white text-slate-800">
+        <div className="personal-section">
+          <div className="container">
+            <h1 id="name">John Daniel Smith</h1>
+          </div>
+          <div className="container">
+            <ul className="contact-list">
+              <li id="email">john-daniel-smith@me.com</li>
+              <li id="phone-number">+44 (0) 7897897897</li>
+              <li id="website">www.john-d-smith.me</li>
+            </ul>
+          </div>
+          <div className="container">
+            <p id="about">
+              Brief introduction of yourself and your motivations...
+            </p>
+          </div>
         </div>
       </div>
     </>
