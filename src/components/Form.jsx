9f0c7fd24c0515b20f77d4fa0c77459a35289c8e
Owen Lewis <owenalewis@gmail.com>
2024-04-10 13:35:18 +0100

    [feat] Add a Form component for better structuring - Add Form component and import the Personal component. - This will be used to organise and contain the different form   components like "Personal".

---
+++ src/components/Form.jsx
@@ -0,0 +1,13 @@
+import Personal from './components/Personal';
+
+function Form() {
+  return (
+    <>
+      <div className="form-container flex flex-col content-start">
+        <Personal />
+      </div>
+    </>
+  );
+}
+
+export default Form;
