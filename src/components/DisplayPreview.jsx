6e3e88cdf62bf5c1251986f930eb258aec96f4ef
Owen Lewis <owenalewis@gmail.com>
2024-04-14 12:55:57 +0200

    [feat] Integrate dynamic display in DisplayPreview
    - Enhance DisplayPreview to dynamically render personal details from
      props.
    - Implement PropTypes for type checking in DisplayPreview to ensure data
      integrity.
    - Ensure real-time preview updates when form data in Personal component
      is submitted.

--- src/components/DisplayPreview.jsx
+++ src/components/DisplayPreview.jsx
@@ -1,23 +1,23 @@
-function DisplayPreview() {
+import PropTypes from 'prop-types';
+
+function DisplayPreview({ personalDetails }) {
   return (
     <>
       <div id="cv-container" className="mx-10">
         <div id="cv" className="w-a4 h-a4 bg-white text-slate-800">
           <div className="personal-section">
             <div className="container">
-              <h1 id="name">John Daniel Smith</h1>
+              <h1 id="name">{personalDetails.name}</h1>
             </div>
             <div className="container">
               <ul className="contact-list">
-                <li id="email">john-daniel-smith@me.com</li>
-                <li id="phone-number">+44 (0) 7897897897</li>
-                <li id="website">www.john-d-smith.me</li>
+                <li id="email">{personalDetails.email}</li>
+                <li id="phone-number">{personalDetails.telephone}</li>
+                <li id="website">{personalDetails.website}</li>
               </ul>
             </div>
             <div className="container">
-              <p id="about">
-                Brief introduction of yourself and your motivations...
-              </p>
+              <p id="about">{personalDetails.about}</p>
             </div>
           </div>
         </div>
@@ -25,5 +25,15 @@ function DisplayPreview() {
     </>
   );
 }
+
+DisplayPreview.propTypes = {
+  personalDetails: PropTypes.shape({
+    name: PropTypes.string,
+    email: PropTypes.string,
+    telephone: PropTypes.string,
+    website: PropTypes.string,
+    about: PropTypes.string,
+  }).isRequired,
+};
 
 export default DisplayPreview;
