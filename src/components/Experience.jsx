810e69d08e376c29eeb58be76f2b385d19c45163
Owen Lewis <owenalewis@gmail.com>
2024-04-30 12:00:50 +0200

    [feat] Build component for dynamic form rendering
    Implement Experience component to dynamically render form groups
    based on the 'type' prop (Educational or Professional). Use switch
    statement to conditionally render relevant form inputs for each type.

---
+++ src/components/Experience.jsx
@@ -0,0 +1,38 @@
+import FormGroup from './FormGroup';
+import PropTypes from 'prop-types';
+
+function Experience({ type }) {
+  let formGroups;
+
+  switch (type) {
+    case 'Educational':
+      formGroups = (
+        <>
+          <FormGroup label="Institute:" type="text" id="institute" />
+          <FormGroup label="Degree:" type="text" id="degree" />
+          {/* Add more form groups for educational experience */}
+        </>
+      );
+      break;
+    case 'Professional':
+      formGroups = (
+        <>
+          <FormGroup label="Company:" type="text" id="company" />
+          <FormGroup label="Position:" type="text" id="position" />
+          {/* Add more form groups for professional experience */}
+        </>
+      );
+      break;
+    default:
+      formGroups = <div>Invalid type: {type}</div>;
+      break;
+  }
+
+  return <div className="experience-section flex flex-row">{formGroups}</div>;
+}
+
+Experience.propTypes = {
+  type: PropTypes.string.isRequired,
+};
+
+export default Experience;
