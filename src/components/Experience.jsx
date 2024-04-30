859f7e4e5536ddb23e8a072a20598c45bce40dd4
Owen Lewis <owenalewis@gmail.com>
2024-04-30 12:18:30 +0200

    [feat] Manage Experience form values with state Implement state and change handling logic in Experience component. Use useState hook to manage formValues state object. Update formValues on input changes via handleChange function passed to FormGroup components.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,15 +1,37 @@
 import FormGroup from './FormGroup';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function Experience({ type }) {
+  const [formValues, setFormValues] = useState({});
+
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setFormValues((prevState) => ({ ...prevState, [id]: value }));
+  };
+
   let formGroups;
 
   switch (type) {
     case 'Educational':
       formGroups = (
         <>
-          <FormGroup label="Institute:" type="text" id="institute" />
-          <FormGroup label="Area of study:" type="text" id="study" />
+          <FormGroup
+            label="Institute:"
+            type="text"
+            id="institute"
+            value={formValues.institute}
+            onChange={handleChange}
+            required={true}
+          />
+          <FormGroup
+            label="Area of study:"
+            type="text"
+            id="study"
+            value={formValues.study}
+            onChange={handleChange}
+            required={true}
+          />
           {/* Add more form groups for educational experience */}
         </>
       );
@@ -17,8 +39,22 @@ function Experience({ type }) {
     case 'Professional':
       formGroups = (
         <>
-          <FormGroup label="Company:" type="text" id="company" />
-          <FormGroup label="Position:" type="text" id="position" />
+          <FormGroup
+            label="Company:"
+            type="text"
+            id="company"
+            value={formValues.company}
+            onChange={handleChange}
+            required={true}
+          />
+          <FormGroup
+            label="Position:"
+            type="text"
+            id="position"
+            value={formValues.position}
+            onChange={handleChange}
+            required={true}
+          />
           {/* Add more form groups for professional experience */}
         </>
       );
